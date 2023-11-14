f96041e7cf1afa4519160ebe2b471dacfc2ef99f
dkenziee <145787315+dkenziee@users.noreply.github.com>
2023-11-15 01:13:59 +0700

    Add files via upload

---
+++ sturktur kendali 2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 01:02:26 2023
+
+@author: ASUS
+"""
+
+for i in range(5):
+    print("Nilai i Adalah ", i)
+for i in range (3,10):
+    print("Kuulangi Sebanyak ", i, "kali")
+for i in range (30,20,-2):
+    print("Aku mundur dari 30", i)
